58f4db7109c9beabc880a4c28a275c6951e65e1b
Kevin <kevinwang2400@gmail.com>
2015-12-16 09:54:19 -0800

    ui testing

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,11 @@ import Voting from './components/Voting';
 const pair = ['Trainspotting', '28 Days Later'];
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+  <Voting pair={pair} hasVoted="Trainspotting"/>,
+  document.getElementById('app')
+);
+
+ReactDOM.render(
+  <Voting pair={pair} winner="Trainspotting"/>,
   document.getElementById('app')
 );
