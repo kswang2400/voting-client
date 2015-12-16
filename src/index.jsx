e1cc499c90998b7b242beb4971867e752bc5d0e9
Kevin <kevinwang2400@gmail.com>
2015-12-16 10:08:47 -0800

    some routes

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Router, { Route } from 'react-router';
+
+import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
 
 const pair = ['Trainspotting', '28 Days Later'];
 
-ReactDOM.render(
-  <Voting pair={pair} hasVoted="Trainspotting"/>,
-  document.getElementById('app')
+const routes = (
+  <Route component={App}>
+    <Route path='/results' component={Results} />
+    <Route path='/' component={Voting} />
+  </Route>
 );
 
 ReactDOM.render(
-  <Voting pair={pair} winner="Trainspotting"/>,
-  document.getElementById('app')
+  <Router>{routes}</Router>, document.getElementById('app')
 );
+
+// ReactDOM.render(
+//   <Voting pair={pair} hasVoted="Trainspotting"/>,
+//   document.getElementById('app')
+// );
+
+// ReactDOM.render(
+//   <Voting pair={pair} winner="Trainspotting"/>,
+//   document.getElementById('app')
+// );
